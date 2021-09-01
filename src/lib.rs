aa8df09fb9b21982b73543b255e7fa7c12fd3216
Oscar te Giffel <oscar@tegiffel.com>
2021-09-01 14:27:10 +0200

    fix: moved test to test mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use crate::List::Cons;
+
 mod drop;
 mod iterator;
 mod lifetime;
@@ -13,11 +15,26 @@ fn internal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
 
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
+    use crate::List::Nil;
 
+    #[test]
     fn internal() {
         assert_eq!(4, internal_adder(2, 2));
     }
@@ -29,5 +46,10 @@ mod tests {
         } else {
             Err(String::from("does not equal"))
         }
+    }
+
+    #[test]
+    fn test_cons() {
+        let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::from(Nil))))));
     }
 }
