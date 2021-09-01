78394fa1f629b2fe941d162347b95c00a2f13592
Oscar te Giffel <oscar@tegiffel.com>
2021-09-02 01:26:52 +0200

    feat: added spawning of tokio futures for handling multiple connections

--- chat-tokio/src/main.rs
+++ chat-tokio/src/main.rs
@@ -5,21 +5,30 @@ use tokio::net::TcpListener;
 async fn main() {
     let listener = TcpListener::bind("localhost:7000").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
-    let (reader, mut writer) = socket.split();
-
-    let mut reader = BufReader::new(reader);
-    let mut line = String::new();
-
     loop {
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
-        if bytes_read == 0 {
-            break;
-        }
+        // To avoid blocking on the "task" level
+        // Task are units of async computations
+        // Like lightweight threads
+        // Tokio schedules the tasks for the most efficient processing
+        // Rust has async block instead of async functions like Javascript, could be writen as async function but this is fine.
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
 
-        writer.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+
+                if bytes_read == 0 {
+                    break;
+                }
+
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
