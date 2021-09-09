0bf62c5b75424082a76e1ad0d21c6f80a355623a
Oscar te Giffel <oscar@tegiffel.com>
2021-09-09 12:56:26 +0200

    wip: deference chapter

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -1,5 +1,46 @@
-#[test]
-fn test_box() {
-    let b = Box::new(5);
-    println!("b = {}", b);
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::smart_pointers::MyBox;
+
+    #[test]
+    fn test_box() {
+        let b = Box::new(5);
+        println!("b = {}", b);
+    }
+
+    #[test]
+    fn test_deference_operator() {
+        let x = 5;
+        let y = &x;
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
+
+    #[test]
+    fn test_my_box() {
+        let x = 5;
+        let y = MyBox::new(x);
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
 }
