acef84412f0fa14a1339adfdb2c3ae2e6e255364
Oscar te Giffel <oscar@tegiffel.com>
2021-10-29 13:38:44 +0200

    feat: added state pattern to be force the compiler to check if the iterator has a bound and only then be able to call with_delims

--- type-driven-api/src/main.rs
+++ type-driven-api/src/main.rs
@@ -2,49 +2,89 @@ use std::{collections::binary_heap::Iter, thread::sleep, time::Duration};
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
-struct Progress<Iter> {
-    iter: Iter,
-    i: usize,
-    bound: Option<usize>,
+struct Unbounded;
+struct Bounded {
+    bound: usize,
+    delims: (char, char),
 }
 
-impl<Iter> Progress<Iter> {
+struct Progress<Iter, Bound> {
+    iter: Iter,
+    i: usize,
+    bound: Bound,
+}
+
+trait ProgressDisplay: Sized {
+    fn display<Iter>(&self, progress: &Progress<Iter, Self>);
+}
+
+impl ProgressDisplay for Unbounded {
+    fn display<Iter>(&self, progress: &Progress<Iter, Self>) {
+        println!("{}", "*".repeat(progress.i));
+    }
+}
+
+impl ProgressDisplay for Bounded {
+    fn display<Iter>(&self, progress: &Progress<Iter, Self>) {
+        println!(
+            "{}{}{}{}",
+            self.delims.0,
+            "*".repeat(progress.i),
+            " ".repeat(self.bound - progress.i),
+            self.delims.1
+        );
+    }
+}
+
+impl<Iter> Progress<Iter, Unbounded> {
     pub fn new(iter: Iter) -> Self {
         Progress {
             iter,
             i: 0,
-            bound: None,
+            bound: Unbounded,
         }
     }
 }
 
-impl<Iter> Progress<Iter>
+impl<Iter> Progress<Iter, Unbounded>
 where
     Iter: ExactSizeIterator,
 {
-    pub fn with_bound(mut self) -> Self {
-        self.bound = Some(self.iter.len());
+    pub fn with_bound(mut self) -> Progress<Iter, Bounded> {
+        let bound = Bounded {
+            bound: self.iter.len(),
+            delims: ('[', ']'),
+        };
+
+        Progress {
+            i: self.i,
+            iter: self.iter,
+            bound,
+        }
+
+        // self.bound = Some(self.iter.len());
+        // self
+    }
+}
+
+impl<Iter> Progress<Iter, Bounded> {
+    pub fn with_delims(mut self, delims: (char, char)) -> Self {
+        self.bound.delims = delims;
         self
     }
 }
 
-impl<Iter> Iterator for Progress<Iter>
+impl<Iter, Bound> Iterator for Progress<Iter, Bound>
 where
     Iter: Iterator,
+    Bound: ProgressDisplay,
 {
     type Item = Iter::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
         println!("{}", CLEAR);
+        self.bound.display(&self);
 
-        match self.bound {
-            Some(bound) => {
-                println!("[{}{}]", "*".repeat(self.i), " ".repeat(bound - self.i))
-            }
-            None => {
-                println!("{}", "*".repeat(self.i))
-            }
-        }
         self.i += 1;
 
         self.iter.next()
@@ -52,11 +92,11 @@ where
 }
 
 trait ProgressIteratorExt: Sized {
-    fn progress(self) -> Progress<Self>;
+    fn progress(self) -> Progress<Self, Unbounded>;
 }
 
 impl<Iter> ProgressIteratorExt for Iter {
-    fn progress(self) -> Progress<Self> {
+    fn progress(self) -> Progress<Self, Unbounded> {
         Progress::new(self)
     }
 }
@@ -66,12 +106,27 @@ fn expensive_calculation(_n: &i32) {
 }
 
 fn main() {
+    let brackets = ('<', '>');
+    // Compile error the iterator does not implement the ExactSizeIterator so it has no bound
+    // Rely on the generic error message for this as a trade of
+    // The error is caught and cant be found at runtime
+
+    // for n in (0..).progress().with_bound() {
+    // expensive_calculation(&n)
+    // }
+
+    //
+
     let v = vec![1, 2, 3];
     for n in v.iter().progress() {
         expensive_calculation(n);
     }
 
-    for n in v.iter().progress().with_bound() {
+    for n in v.iter().progress().with_bound().with_delims(brackets) {
         expensive_calculation(n);
     }
+
+    // for n in (0..).progress().with_delims(brackets) {
+    // expensive_calculation(&n)
+    // }
 }
