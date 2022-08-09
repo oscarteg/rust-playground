f65582f5e70a34094bd0f26c803efaabe07769be
Oscar te Giffel <oscar@tegiffel.com>
2022-08-09 12:48:46 +0200

    feat: added start of webserver

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,18 +1,39 @@
+#![allow(dead_code)]
 use tokio::{
     io::AsyncWriteExt,
     net::{TcpListener, TcpStream},
 };
 
-use std::io;
+enum Method {
+    GET,
+    HEAD,
+    POST,
+    DELETE,
+    TRACE,
+    CONNECT,
+}
 
-async fn process_socket(mut socket: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
-    let mut buf = [0; 1024];
-    socket.write_all(b"hello world").await?;
+struct Request {
+    method: Method,
+}
+
+async fn process_socket(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    let mut buffer = [0; 1024];
+
+    // stream.(&mut buffer).unwrap();
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).await?;
+
+    stream.flush().await?;
     Ok(())
 }
 
 const IP: &str = "127.0.0.1:8081";
 
+/// Method Request-URI HTTP-Version CRLF
+/// headers CRLF
+/// message-body
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind(IP).await?;
