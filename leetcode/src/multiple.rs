4b0b06dd749eb979c9c53c16e504af057e549c81
Oscar te Giffel <oscar@tegiffel.com>
2023-05-06 22:27:54 +0200

    feat: added 2 leetcode problems (easy)

--- leetcode/src/multiple.rs
+++ leetcode/src/multiple.rs
@@ -15,8 +15,47 @@ impl Solution {
     }
 }
 
+impl Solution {
+    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+        let mut sum = 0;
+        let v: Vec<i32> = Vec::new();
+        nums.into_iter()
+            .map(|v| {
+                sum += v;
+                sum
+            })
+            .collect::<Vec<i32>>()
+    }
+}
+
+impl Solution {
+    // Loop and check the remaining expected
+    pub fn pivot_index(nums: Vec<i32>) -> i32 {
+        // First loop to create total sum
+        let total_sum: i32 = nums.iter().sum();
+
+        let mut left_sum = 0;
+
+        // loop until end of array
+        for i in 0..nums.len() {
+            if left_sum == total_sum - left_sum - nums[i] {
+                return i as i32;
+            }
+            left_sum += nums[i]
+        }
+
+        -1
+    }
+}
+
 #[cfg(test)]
 mod tests {
+
+    #[test]
+    fn pivot_index() {
+        assert_eq!(super::Solution::pivot_index(vec![1, 7, 3, 6, 5, 6]), 3);
+    }
+
     #[test]
     fn test() {
         let input = vec![
